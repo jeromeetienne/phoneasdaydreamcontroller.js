7da80409c9bb1f178b3b39fa40e9d4e7f2a8308c
Jerome Etienne <jerome.etienne@gmail.com>
2016-11-06 20:48:30 +0000

    added .gitignore

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # phonegamepad
-use your phone as a gamepad (3dof Virtual reality gamepad)
+use your phone as a gamepad (3dof Virtual reality gamepad).
+
+It is quite early stage, looking forward to have that in the ray-input
+
+
+# Instruction
+- start the websocket server ```node index.js```
+- enter the url of the server on your phone, e.g. ```http://192.168.0.2:3000```
+  - you will see the gamepad page
+- start a static http server, e.g. ```http-server```
+- open ```http:127.0.0.1:8080/examples/webvr.html``` for an example of 3dof controller
---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+        res.sendfile('./phone.html');
+});
+
+io.on('connection', function(socket){
+        console.log('a user connected');
+        socket.on('disconnect', function(){
+                console.log('user disconnected');
+        });
+        socket.on('chat message', function(msg){
+                console.log('broadcast', msg)
+                io.emit('chat message', msg);
+        });
+});
+
+
+http.listen(3000, function(){
+        console.log('listening on *:3000');
+});
