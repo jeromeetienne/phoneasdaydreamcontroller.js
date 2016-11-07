a6beb1d39f67c58166e5e91806805e75f36be697
Jerome Etienne <jerome.etienne@gmail.com>
2016-11-07 01:39:52 +0000

    cleanup

---
+++ phoneasvrcontroller.js
@@ -0,0 +1,103 @@
+function PhoneAsVRController(){
+	var socket = io('http://127.0.0.1:3000');
+	// general spec for Gamepad API - https://www.w3.org/TR/gamepad/
+	var gamepad = {
+		'id' : 'PhoneAsGamepad.js controller',
+		'index' : 0,
+		'connected' : true,
+		'mapping' : 'standard',
+		'axes' : [],
+		'buttons' : [{
+			'pressed' : false,
+			'touched' : false,
+			'value' : 0,
+		}, {
+			'pressed' : false,
+			'touched' : false,
+			'value' : 0,		
+		}],
+		// VR extension - https://w3c.github.io/gamepad/extensions.html#dom-gamepadpose
+		'pose' : {
+			'hasPosition' : false,
+			'hasOrientation' : true,
+			
+			'position' : null,
+			'linearVelocity' : null,
+			'linearAcceleration' : null,
+			
+			'orientation' : [0,0,0,1],
+			'angularVelocity' : null,
+			'angularAcceleration' : null
+		}
+	}
+	this.gamepad = gamepad
+
+	var firstAngle = null
+	socket.on('broadcast', function(message){
+	        // console.log('received', message)
+	// return        
+		var event = JSON.parse(message)
+		if( event.type === 'deviceOrientation' ){
+	                // console.log('broadcast', message)
+			if( firstAngle === null ){
+				firstAngle = {
+					alpha: event.alpha,
+					beta: event.beta,
+					gamma: event.gamma
+				}
+	                        return
+			}else{
+				var alpha = event.alpha-firstAngle.alpha
+	                        var beta = event.beta-firstAngle.beta
+	                        var gamma = event.gamma-firstAngle.gamma
+			}
+			var euler = new THREE.Euler()
+			euler.x =  beta  / 180 * Math.PI
+			euler.y =  alpha / 180 * Math.PI
+			euler.z = -gamma / 180 * Math.PI
+			euler.order = "YXZ"
+			
+			var quaternion = new THREE.Quaternion()
+			quaternion.setFromEuler(euler)
+	                quaternion.toArray(gamepad.pose.orientation)
+		}else if( event.type === 'touchstart' ){
+			if( event.target === 'appButton' ){
+				gamepad.buttons[0].pressed = true
+				gamepad.buttons[0].value = 1
+			}else if( event.target === 'homeButton' ){
+				gamepad.buttons[1].pressed = true
+				gamepad.buttons[1].value = 1
+			}else{
+				console.assert(false)
+			}
+		}else if( event.type === 'touchend' ){
+			if( event.target === 'appButton' ){
+				gamepad.buttons[0].pressed = false
+				gamepad.buttons[0].value = 0
+			}else if( event.target === 'homeButton' ){
+				gamepad.buttons[1].pressed = false
+				gamepad.buttons[1].value = 0
+			}else{
+				console.assert(false)
+			}
+
+		}else {
+			console.assert(false)
+		}
+	})	
+}
+
+;(function(){
+	var phoneAsVRController = new PhoneAsVRController()
+	navigator.getGamepads = function(){
+		// console.log('getGamepads')
+        	var gamepads = [ 
+			phoneAsVRController.gamepad,
+			undefined,
+			undefined,
+			undefined,
+		]
+		return gamepads
+	}
+	
+})()
---
+++ server.js
@@ -0,0 +1,40 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//////////////////////////////////////////////////////////////////////////////
+//                export static files
+//////////////////////////////////////////////////////////////////////////////
+app.get('/', function(req, res){
+        res.sendfile('./phone.html');
+});
+app.get('/phone.html', function(req, res){
+        res.sendfile('./phone.html');
+});
+app.get('/phoneasvrcontroller.js', function(req, res){
+        res.sendfile('./phoneasvrcontroller.js');
+});
+
+
+//////////////////////////////////////////////////////////////////////////////
+//                handle socket.io
+//////////////////////////////////////////////////////////////////////////////
+io.on('connection', function(socket){
+        console.log('a user connected');
+        socket.on('disconnect', function(){
+                console.log('user disconnected');
+        });
+        socket.on('broadcast', function(msg){
+                console.log('broadcast', msg)
+                io.emit('broadcast', msg);
+        });
+});
+
+
+//////////////////////////////////////////////////////////////////////////////
+//                start listening 
+//////////////////////////////////////////////////////////////////////////////
+
+http.listen(3000, function(){
+        console.log('listening on *:3000');
+});
